6ea491c7b7b2b5262886dd52959c3a4dca711fba
Abdulwasiu Apalowo <bazzanapalowo@gmail.com>
2022-05-04 12:10:09 +0100

    products: /api/products
        - Register products app with the main admin
        - Update README.md
        - Link /api/product endpoint to the appropriate class.

--- README.md
+++ README.md
@@ -1,9 +1,88 @@
 
-## ECOMMERCE APPLICATION
+# ECOMMERCE APPLICATION
 
 The application is containerized in a docker-compose.yaml file.
 
 Tools: `podman` and `python`
+
+#Endpoints:
+
+
+Products
+- 
+  - **Endpoint** ``GET`` `/api/products`
+  - Get all products. 
+  - It takes no parameter.
+  - **STATUS CODE** ``200``
+
+
+- 
+    - **Endpoint** ``POST`` `/api/products`
+    - Create a Product.
+      - **Body**: A `json` object.
+      - This is required.
+      - Example: 
+        ```json
+          {
+            "title": "ade",
+            "image": "old image"
+          }
+        ```
+  - **STATUS CODE** ``201``
+
+
+- 
+  - **Endpoint** ``GET`` `/api/products/<id>`
+  - Get product with specific id.
+  - **id**: string
+    - The ID of the element. It is required.
+    - example: 1 
+  - **STATUS CODE** ``200``
+
+
+- 
+  - **Endpoint** ``PUT`` `/api/products/<id>`
+  - Update all the fields in product.
+  - **id**: string
+    - The ID of the element. It is required.
+    - example: 1 
+  - **Body**: A `json` object.
+      - This is required.
+      - Example: 
+        ```json
+          {
+            "title": "ade",
+            "image": "old image"
+          }
+        ```
+  - **STATUS CODE** ``202``
+
+
+- 
+  - **Endpoint** ``PATCH`` `/api/products/<id>`
+  - Update partial data for product.
+  - **id**: string
+    - The ID of the element. It is required.
+    - example: 1 
+  - **Body**: A `json` object.
+      - This is required.
+      - Example: 
+        ```json
+          {
+            "title": "ade"
+          }
+        ```
+  - **STATUS CODE** ``202``
+
+
+- 
+  - **Endpoint** ``DELETE`` `/api/products/<id>`
+  - Delete product with id.
+  - **id**: string
+    - The ID of the element. It is required.
+    - example: 1
+  - **STATUS CODE** ``204``
+
 
 To get started:
 -
--- admin/products/views.py
+++ admin/products/views.py
@@ -1,5 +1,6 @@
-from django.shortcuts import render
-from rest_framework import viewsets
+from django.shortcuts import get_object_or_404
+from rest_framework import viewsets, status
+from rest_framework.response import Response
 
 from .serializer import ProductSerializer
 from .models import Product
@@ -7,16 +8,42 @@ from .models import Product
 
 class ProductViewSet(viewsets.ViewSet):
     def list(self, request):
-        pass
+        products = Product.objects.all()
+        serializer = ProductSerializer(products, many=True)
+        return Response(serializer.data, status=status.HTTP_200_OK)
 
     def create(self, request):
-        pass
+        serializer = ProductSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+    @staticmethod
+    def get_product(pk=None):
+        return get_object_or_404(Product, pk=pk)
 
     def retrieve(self, request, pk=None):
-        pass
+        product = self.get_product(pk)
+        serializer = ProductSerializer(product)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+
+    def _update(self, request, pk, partial=False):
+        product = self.get_product(pk)
+        serializer = ProductSerializer(instance=product, data=request.data, partial=partial)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
 
     def update(self, request, pk=None):
-        pass
+        return self._update(request, pk=pk)
+
+    def partial_update(self, request, pk=None):
+        return self._update(request, pk=pk, partial=True)
 
     def destroy(self, request, pk=None):
-        pass
+        product = self.get_product(pk)
+        product.delete()
+        return Response(
+            {"delete": f"Product(id={pk}) successfully deleted."},
+            status=status.HTTP_204_NO_CONTENT
+        )
